54075a6dcedea642ea12c587ee92125ad9acf7c2
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2022-04-16 11:42:55 +1200

    Update pxt.json, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "puppy1":return img`
+. . d d d 4 . . . . . . 
+. e 9 d 9 d e e . . . . 
+. e e 3 e d 3 e . . 4 . 
+. . e f d d . 4 . . . e 
+. . f 4 f 4 . . . . e e 
+. e e e e e e e e e e . 
+. e e e e 4 4 e e 4 e . 
+. 4 e e e e e e e e e . 
+. . e e e e e e e 4 e . 
+. . e . . e . . e . e . 
+. . e . . 4 . . e . e . 
+. f e . f e . f e f e . 
+`;
+            case "image2":
+            case "Dog 1":return img`
+. e e e e . . . . . . . 
+e 9 e 9 e e . . . . . . 
+3 e 3 e e 3 e . . . . e 
+. e f e e d d d d e e . 
+. d d d d d d d e e . . 
+. d d d d d e e e e . . 
+. d d d e e e e e e . . 
+. d d e e e e e e e . . 
+. d . d . . . d . d . . 
+. d . d . . . d . d . . 
+f d f d . . f d f d . . 
+. . . . . . . . . . . . 
+`;
+            case "image3":
+            case "kitten":return img`
+. . 4 4 . 4 4 . . . . . 
+. . 3 4 4 3 4 . . . . . 
+. . 4 5 4 5 4 . . . . . 
+. . 4 4 3 4 4 . . . f . 
+. . 4 3 4 3 4 . . . . f 
+. . 4 4 4 4 4 4 4 4 4 f 
+. . 4 4 f 4 4 f 4 4 4 f 
+. . 4 f f f 4 f 4 4 4 4 
+. . 4 4 f 4 4 4 4 4 4 4 
+. 4 . . 4 . . 4 . . 4 . 
+. 4 . . 4 . . 4 . . 4 . 
+1 4 . 1 4 . 1 4 . 1 4 . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
