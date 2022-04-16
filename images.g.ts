b81c730176265d7fd73db693d2672a134bc3bc27
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2022-04-16 12:16:13 +1200

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,51 @@ namespace myImages {
 . . . . f f f f f f . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
+            case "ZST]|as4ozz}BOxY@%7*":
+            case "dog2":return img`
+. e e e e . . . . . . . 
+e 9 e 9 e e . . . . . . 
+3 e 3 e e 3 e . . . . e 
+. e f e e d d d d e e . 
+. d d d d d d d e e . . 
+. d d d d d e e e e . . 
+. d d d e e e e e e . . 
+. d d e e e e e e e . . 
+. d . d . . . d . d . . 
+. d . d . . . d . d . . 
+f d f d . . f d f d . . 
+. . . . . . . . . . . . 
+`;
+            case "gpfvnX8/H;3mZzY4#[Dp":
+            case "puppy1":return img`
+. . d d d 4 . . . . . . 
+. e 9 d 9 d e e . . . . 
+. e e 3 e d 3 e . . 4 . 
+. . e f d d . 4 . . . e 
+. . f 4 f 4 . . . . e e 
+. e e e e e e e e e e . 
+. e e e e 4 4 e e 4 e . 
+. 4 e e e e e e e e e . 
+. . e e e e e e e 4 e . 
+. . e . . e . . e . e . 
+. . e . . 4 . . e . e . 
+. f e . f e . f e f e . 
+`;
+            case "];zRNM":
+            case "kitten1":return img`
+. . 4 4 . 4 4 . . . . . 
+. . 3 4 4 3 4 . . . . . 
+. . 4 5 4 5 4 . . . . . 
+. . 4 4 3 4 4 . . . f . 
+. . 4 3 4 3 4 . . . . f 
+. . 4 4 4 4 4 4 4 4 4 f 
+. . 4 4 f 4 4 f 4 4 4 f 
+. . 4 f f f 4 f 4 4 4 4 
+. . 4 4 f 4 4 4 4 4 4 4 
+. 4 . . 4 . . 4 . . 4 . 
+. 4 . . 4 . . 4 . . 4 . 
+1 4 . 1 4 . 1 4 . 1 4 . 
+`;
         }
         return null;
     })
